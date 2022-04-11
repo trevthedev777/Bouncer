9eda28f51c54c3855fe98f7b9e6283782eb86d92
Your Name <you@example.com>
2022-04-11 11:27:48 +0200

    Fix bug on empty string declaration

--- bouncer.py
+++ bouncer.py
@@ -1,12 +1,17 @@
 # Ask For Age
+print('Please enter an age so we can allocate you your place in the party')
 age = input('How Old Are you?\n')
 
-if int(age) >= 18 and int(age) <= 21:
-    print('You Can Enter But you Need A Wrist Band')
-elif int(age) >= 21:
-    print('Enjoy The Party!:)')
+if age != '':
+    if int(age) >= 18 and int(age) <= 21:
+        # 18 - 21 Writband
+        print('You Can Enter But you Need A Wrist Band')
+    elif int(age) >= 21:
+        # 21+ drink, normal entry
+        print('Enjoy The Party!:)')
+    else:
+        # too young
+        print("You Can't Enter As You Are Underage")
 else:
-    print("You Can't Enter As You Are Underage")
-# 18 - 21 Writband
-# 21+ drink, normal entry
-# too young
+    print('Please enter an age')
+
