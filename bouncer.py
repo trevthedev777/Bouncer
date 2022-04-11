d2bf6c7b841b6a030b45c47c0b7ccde44d072e3e
Your Name <you@example.com>
2022-04-11 11:20:55 +0200

    Add Conditionals for age selection

---
+++ bouncer.py
@@ -0,0 +1,9 @@
+# Ask For Age
+age = input('How Old Are you?')
+
+if int(age) >= 18 and int(age) <= 21:
+    print('You Can Enter But you Need A Wrist Band')
+
+# 18 - 21 Writband
+# 21+ drink, normal entry
+# too young
