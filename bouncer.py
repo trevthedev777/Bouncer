360400a1d8c8c0c9e3d6437e11abfc73532935ba
Your Name <you@example.com>
2022-04-11 11:24:59 +0200

    Add all conditionals according to age

--- bouncer.py
+++ bouncer.py
@@ -1,9 +1,12 @@
 # Ask For Age
-age = input('How Old Are you?')
+age = input('How Old Are you?\n')
 
 if int(age) >= 18 and int(age) <= 21:
     print('You Can Enter But you Need A Wrist Band')
-
+elif int(age) >= 21:
+    print('Enjoy The Party!:)')
+else:
+    print("You Can't Enter As You Are Underage")
 # 18 - 21 Writband
 # 21+ drink, normal entry
 # too young
